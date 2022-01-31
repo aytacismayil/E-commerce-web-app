7b3f693f1fb42d00a9562b352812fdc765487161
aytacismayil <aytaj.ismayill@gmail.com>
2022-02-01 00:36:14 +0400

    firebase

---
+++ src/fireConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrzB0V8gAw-zF_kjyL4Zx28-N20-4yLAc",
+  authDomain: "firecommerce-73784.firebaseapp.com",
+  projectId: "firecommerce-73784",
+  storageBucket: "firecommerce-73784.appspot.com",
+  messagingSenderId: "933945324162",
+  appId: "1:933945324162:web:d4aae06243b58b11befa9b",
+  measurementId: "G-SZ772L0D61",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const fireDB = getFirestore(app);
+
+export default fireDB;
